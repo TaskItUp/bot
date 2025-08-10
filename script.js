2d97a06979095643c609a39fe25410ec13944d09
TaskItUp <westonremington0@gmail.com>
2025-08-10 16:42:50 +0530

    script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const db = firebase.firestore();
 let userState = {};
 let telegramUserId = null;
 let isInitialized = false;
-const TELEGRAM_BOT_USERNAME = "TaskItUpBot";
+const TELEGRAM_BOT_USERNAME = "Pepetesedyfbot";
 
 const DAILY_TASK_LIMIT = 40;
 const AD_REWARD = 250;
@@ -169,3 +169,4 @@ document.addEventListener('DOMContentLoaded', () => {
         initializeApp(null);
     }
 });
+
